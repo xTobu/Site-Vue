fab6830c3f2803c80535abd940e8cff915e5f933
Junxiang <junxiang.oriint@gmail.com>
2018-03-01 22:40:23 +0800

    更新樣式與favicon

--- src/store/getters/index.js
+++ src/store/getters/index.js
@@ -1,7 +1,7 @@
 export default {
 	Count: state => state.count,
 	doubleCount: state => {
-		return state.count * 3;
+		return state.count * 2;
 	},
 	getterStudent: state => {
 		return state.student;
